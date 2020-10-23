72ced300b3299efc99e01272c9a439b5ab570842
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-23 02:21:59 +0000

    Deploy kantahusari/SPA_week05_comp3104_lab_exec to github.com/kantahusari/SPA_week05_comp3104_lab_exec.git:gh-pages

---
+++ precache-manifest.c8ceb14d2199720a2110b0cb3cb953e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23634334560d906fe02f",
+    "url": "/static/css/main.93af1c10.chunk.css"
+  },
+  {
+    "revision": "23634334560d906fe02f",
+    "url": "/static/js/main.23634334.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "48f0500884e86d7bfe4bfb9b386cf14e",
+    "url": "/index.html"
+  }
+];
